fb25b186900344c942d21dc3427e2477b304ee52
michal <zelo21@wp.pl>
2017-10-31 19:12:42 +0100

    sqlite fixed xd

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^admin/', admin.site.urls),
     #url(r'^login/$',auth_views.login ,{'template_name':'blog/register.html'}, name='login')
 ]
