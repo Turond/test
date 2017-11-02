7a33a3e7c3204f97a13aff0de8189fd8f04aca29
michal <zelo21@wp.pl>
2017-11-02 16:24:05 +0100

    form throws errors now xd

--- blog/views.py
+++ blog/views.py
@@ -4,28 +4,6 @@ from django.utils import timezone
 from django.contrib.auth import authenticate, login, logout
 from .forms import UserLoginForm, UserLoginFormLegit
 # Create your views here.
-def loginp(request):
-    if request.method == 'POST':
-        form = UserLoginFormLegit(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            user_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=user_password)
-            login(request, user)
-            if user is not None:
-                login(request, user)
-                return redirect('post_list')
-            else:
-                form = UserLoginFormLegit()
-                return render(request, 'blog/loginp.html', {'form': form})
-        else:
-            form = UserLoginFormLegit()
-            return render(request, 'blog/loginp.html', {'form': form})
-    else:
-        form = UserLoginFormLegit()
-        return render(request, 'blog/loginp.html', {'form': form})
-
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -44,7 +22,7 @@ def register(request):
             return redirect('post_list')
     else:
         form = UserLoginForm()
-        return render(request, 'blog/register.html', {'form': form})
+    return render(request, 'blog/register.html', {'form': form})
 
 def logout_page(request):
     logout(request)
